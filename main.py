e505f40eca7b9055fb4bc7616483911794bec64d
Spiros <geokh@hotmail.com>
2022-08-31 14:40:16 +0300

    Adding Reset capabilities and fixing some bugs with reset and start timer

--- main.py
+++ main.py
@@ -10,14 +10,38 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_mark_label.config(text=" ")
+    global  reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Long Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Short Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -30,7 +54,16 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
 
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_mark_label.config(text=marks)
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -48,13 +81,13 @@ canvas.create_image(100, 112, image=tomato_img, )
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-check_mark_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=FONT_NAME)
+check_mark_label = Label(fg=GREEN, bg=YELLOW, font=FONT_NAME)
 check_mark_label.grid(column=1, row=2, pady=20)
 
 start_button = Button(text="Start", command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", command=reset_timer ,highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
 window.mainloop()
